4ea43d4bc2df86bbfe2ef7de427836d72c0ef559
essameldeen <38116813+essameldeen@users.noreply.github.com>
2018-12-04 14:50:33 +0200

    dynamic broadCast Receiver

--- app/src/main/java/com/example/essam/broadcast/MainActivity.java
+++ app/src/main/java/com/example/essam/broadcast/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.essam.broadcast;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.essam.broadcast.Utilit.BroadCastReceiver;
 
+public class MainActivity extends AppCompatActivity {
+    // Dynamic brodCast Receiver
+       BroadCastReceiver broadCastReceiver = new BroadCastReceiver();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        registerReceiver(broadCastReceiver,intentFilter);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadCastReceiver);
     }
 }
